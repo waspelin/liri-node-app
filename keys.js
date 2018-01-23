be230d70109b5608ce05417a0e5f952ed1e03e5a
waspelin <waspelin@gmail.com>
2018-01-23 00:05:54 -0800

    more info to make LIRI work with API's

--- keys.js
+++ keys.js
@@ -1,10 +1,23 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'BjYaXuCtOrH7US3sRqc7KxsX4',
+  consumer_secret: 'ApCwptqN2ULf0VJZVA9V1BrFaUCQg6F5PWs9P7sYx13jszMBIP',
+  access_token_key: '1323253838-4du7WhKhTe6xEw46GRehqOEgYOUImX0lfGoxt0v',
+  access_token_secret: 'lmcTBOPi1unz37tGyMkZHCJS04mRCQxQfCI5iVOtMy5wl',
 }
 
 module.exports = twitterKeys;
+
+var spotifyKeys = {
+  consumer_key = '23f682c7b58c4035944fe244c2cb8bb3',
+  consumer_secret = '5b777c494d914b40a9ab6c14e05d9916',
+}
+
+module.exports = spotifyKeys;
+
+var omdbKeys = {
+  key = '40e9cece',
+}
+
+module.exports = omdbKeys;
