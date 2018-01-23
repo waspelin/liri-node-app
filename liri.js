9aefeb5901a7888dab80838c5e0e9560826087ed
waspelin <waspelin@gmail.com>
2018-01-23 00:27:07 -0800

    adding log and first pseudo coding for liri.js

---
+++ liri.js
@@ -0,0 +1,17 @@
+// four command line funtions: "my-tweets" "spotify-this-song" "movie-this" "do-what-it-says"
+
+// my-tweets function: shows your last 20 tweets and when they were created in bash window
+
+
+
+// spotify-this-song function: will show artist, song name, preview link to Spotify, album name in bash window
+// note that if no song is provided, the function will default to "The Sign" by Ace of Base
+
+
+
+// movie-this function: Title, Year of movie, imdb rating, rotten tomatoes rating, country produced in, langauge, plot of the movie, actors in the movie
+// note that if no movie give, the program will output data for the movie "Mr. Nobody"
+
+
+
+// do-what-it-says function: using the 'fs' node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands
